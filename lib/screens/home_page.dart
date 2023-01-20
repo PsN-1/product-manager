7484301b6290fa6e72d642fd52ece0fd14702961
Pedro Neto <pedro.santosnt@hotmail.com>
2023-01-20 15:09:05 -0300

    add image with firebase cloud

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,6 +6,8 @@ import 'package:product_manager/widgets/product_card.dart';
 final _firestore = FirebaseFirestore.instance;
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -14,46 +16,54 @@ class _HomePageState extends State<HomePage> {
   List<Widget> products = [];
 
   @override
-  void initState() {
-    super.initState();
-
-    setState(() {
-      loadProducts();
-    });
-  }
-
-  void loadProducts() async {
-    var data = await _firestore
-        .collection('Products')
-        .withConverter(
-          fromFirestore: Product.fromFirestore,
-          toFirestore: (Product product, _) => product.toFirestore(),
-        )
-        .get();
-
-    for (var docSnap in data.docs) {
-      var product = docSnap.data();
-      products.add(ProductCard(product: product, onTap: () {
-      print(product.product);
-    }));
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView(
-        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        // crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: products, 
-          // ProductCard(
-          //   product: product,
-          //   onTap: () {},
-        //   // )
-        // ],
+      appBar: AppBar(
+          title: const Text(
+        "Controle de Estoque",
+      )),
+      body: const SafeArea(
+        child: ProductsStream(),
       ),
     );
   }
 }
 
+class ProductsStream extends StatelessWidget {
+  const ProductsStream({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Product>>(
+      stream: _firestore
+          .collection('Products')
+          .withConverter(
+            fromFirestore: Product.fromFirestore,
+            toFirestore: (Product product, _) => product.toFirestore(),
+          )
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightGreenAccent,
+            ),
+          );
+        }
+        final products = snapshot.data?.docs;
+        List<ProductCard> productsCards = [];
+
+        for (var productData in products!) {
+          productsCards
+              .add(ProductCard(product: productData.data(), onTap: () {}));
+        }
+
+        return Expanded(
+          child: ListView(
+            children: productsCards,
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,22 +1,50 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:product_manager/models/product.dart';
 
+final _storageRef = FirebaseStorage.instance.ref();
+
+class ImageHelper extends StatelessWidget {
+  String? image;
+
+  ImageHelper({this.image});
+
+  Future<String> getImage() async {
+    final pathReference = _storageRef.child('$image.jpg');
+    final imageUrl = await pathReference.getDownloadURL();
+    return imageUrl;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getImage(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> something) {
+          if (something.hasData) {
+            return Image(image: NetworkImage(something.data));
+          }
+          return const Text('no Image Found');
+        });
+  }
+}
+
 class ProductCard extends StatelessWidget {
   final Product product;
+  final String? image;
   final void Function() onTap;
 
-  const ProductCard({super.key, required this.product, required this.onTap});
+  const ProductCard(
+      {super.key, required this.product, this.image, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-
       onTap: onTap,
       child: Container(
         margin: const EdgeInsets.all(15),
         child: PhysicalModel(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(5),
           elevation: 5,
           child: Padding(
             padding: const EdgeInsets.all(10.0),
@@ -24,6 +52,7 @@ class ProductCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                ImageHelper(image: product.image),
                 Text(
                   product.product ?? "",
                   style: const TextStyle(
