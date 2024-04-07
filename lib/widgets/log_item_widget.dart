a67236d1acccea483d6dfcefc1365bb3818b0390
Pedro Neto <pedro.santosnt@hotmail.com>
2024-04-07 14:13:24 -0300

    improve log display for product creation

--- lib/widgets/log_item_widget.dart
+++ lib/widgets/log_item_widget.dart
@@ -29,7 +29,8 @@ class LogItemWidget extends StatelessWidget {
             style: logTextStyle,
           ),
         ),
-        Text(" - $abbreviation: $oldValue", style: logTextStyle),
+        if (abbreviation != "Obs")
+          Text(" - $abbreviation: $oldValue", style: logTextStyle),
         const Icon(Icons.arrow_right),
         Text(newValue, style: logTextStyle),
       ],
